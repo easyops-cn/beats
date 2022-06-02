a639e50fa69cc6d51476c5df077b4bd00dc4a19b
kestrelcjx <kestrelcjx@gmail.com>
2022-06-02 16:23:29 +0800

    fix(memory): wrong system_memory_actual_free

--- metricbeat/internal/metrics/memory/memory_linux.go
+++ metricbeat/internal/metrics/memory/memory_linux.go
@@ -41,13 +41,14 @@ func get(rootfs string) (Memory, error) {
 	memData := Memory{}
 
 	var free, cached uint64
+	var ok bool
 	if total, ok := table["MemTotal"]; ok {
 		memData.Total = metrics.OptUintWith(total)
 	}
-	if free, ok := table["MemFree"]; ok {
+	if free, ok = table["MemFree"]; ok {
 		memData.Free = metrics.OptUintWith(free)
 	}
-	if cached, ok := table["Cached"]; ok {
+	if cached, ok = table["Cached"]; ok {
 		memData.Cached = metrics.OptUintWith(cached)
 	}
 
